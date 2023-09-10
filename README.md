e77378dfdf8f140a90d1194a1200852b1e48091e
emon-webdev <emon.webdev@gmail.com>
2023-09-10 15:07:48 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# The-Cake-Stand-Server
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+/* 
+theCakeStandDB
+user: cakeStandUser
+padd: BVx7L9vWs0E2k2AS
+*/
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0aavw86.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const menuCollection = client.db("theCakeStandDB").collection("menu");
+        const reviewCollection = client.db("theCakeStandDB").collection("reviews");
+
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/review', async (req, res) => {
+            const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('The cake stand in running')
+})
+
+app.listen(port, () => {
+    console.log(`The cake stand server in running on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
